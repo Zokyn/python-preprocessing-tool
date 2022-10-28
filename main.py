b85fadedda48046886cbaffe3030b4a98247f1a2
Zokyn <gustavo.casaes@mail.uft.edu.br>
2022-10-28 20:11:57 -0300

    correções e organização no código

--- main.py
+++ main.py
@@ -1,32 +1,29 @@
 import numpy as np
 import pandas as pd
 
+max_std = 10
 data = pd.read_csv('winequality-red.csv', sep=';')
 
-def verify_std_column():
-    for col in data.columns: 
-        outliners = False
-        std = data[col].std()
-        if(std >= 1.0):
-            outliners = True
-
-        print(f'{col}: {std} : {outliners}')
-
-def att_column():
+def has_outliners(data, col):   
+    if(data[col].std() >= max_std):
+        return True;
+    else:
+        return False;
+def column_attributes(data):
     list_of_columns = []
     for col in data.columns:
-        attributes = ['mean', 'std', 'max', 'min']
+        attributes = ['mean', 'std', 'max', 'min', 'hasOutliners?']
         column_mean = data[col].mean()
         column_std = data[col].std()
         column_max = data[col].max()
         column_min = data[col].min()
         
-        column = pd.Series([column_mean, column_std, column_max, column_min], index=[attributes], name=col)
+        outliners = has_outliners(data, col)
+
+        column = pd.Series([column_mean, column_std, column_max, column_min, outliners], index=[attributes], name=col)
 
         list_of_columns.append(column)
-        # print(column)
-        # print(f"'{col}': {column_mean} | {column_std} | {column_max} | {column_min}")
-    print(pd.DataFrame(list_of_columns))
+    return pd.DataFrame(list_of_columns)
 
-att_column();
+print(column_attributes(data));
 # print(data)
