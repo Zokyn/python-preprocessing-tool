dd6fb31546e18f043030e1cf7a99b6bb2a6b666f
Zokyn <gustavo.casaes@mail.uft.edu.br>
2022-10-28 20:31:14 -0300

    outras correções elaboradas

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 
 max_std = 10
-data = pd.read_csv('winequality-red.csv', sep=';')
+data = pd.read_csv('winequality-red.csv', sep=';') # dataset
+columns = data.columns; # print(columns) # colunas do dataset
 
 def has_outliners(data, col):   
     if(data[col].std() >= max_std):
@@ -11,19 +12,22 @@ def has_outliners(data, col):
         return False;
 def column_attributes(data):
     list_of_columns = []
+    # Percorre cada coluna do dataset e define seus atributos
     for col in data.columns:
+        # Atributos baseado em propriedades estatísticas de cada coluna
         attributes = ['mean', 'std', 'max', 'min', 'hasOutliners?']
-        column_mean = data[col].mean()
-        column_std = data[col].std()
-        column_max = data[col].max()
-        column_min = data[col].min()
-        
+        column_mean = data[col].mean()  # MEDIA
+        column_std = data[col].std()    # DESVIO PADRÃO    
+        column_max = data[col].max()    # VALOR MAXIMO
+        column_min = data[col].min()    # VALOR MINIMO 
+        # Por ultimo utiliza de uma função para verificar se há outliners
         outliners = has_outliners(data, col)
 
-        column = pd.Series([column_mean, column_std, column_max, column_min, outliners], index=[attributes], name=col)
+        column = pd.Series(
+            data=[column_mean, column_std, column_max, column_min, outliners], 
+            index=[attributes], 
+            name=col
+        )
 
         list_of_columns.append(column)
     return pd.DataFrame(list_of_columns)
-
-print(column_attributes(data));
-# print(data)
