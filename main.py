a6117f97e4265b33d32c03d09c16b8b74f8e125f
Zokyn <gustavo.casaes@mail.uft.edu.br>
2022-10-28 19:43:50 -0300

    primeira versão da tabela de valores estatísticos de cada coluna

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 import numpy as np
 import pandas as pd
 
-path = 'winequality-red.csv';
-s = pd.read_csv(path, sep=';')
+data = pd.read_csv('winequality-red.csv', sep=';')
 
-print(s)
+def verify_std_column():
+    for col in data.columns: 
+        outliners = False
+        std = data[col].std()
+        if(std >= 1.0):
+            outliners = True
+
+        print(f'{col}: {std} : {outliners}')
+
+def att_column():
+    list_of_columns = []
+    for col in data.columns:
+        attributes = ['mean', 'std', 'max', 'min']
+        column_mean = data[col].mean()
+        column_std = data[col].std()
+        column_max = data[col].max()
+        column_min = data[col].min()
+        
+        column = pd.Series([column_mean, column_std, column_max, column_min], index=[attributes], name=col)
+
+        list_of_columns.append(column)
+        # print(column)
+        # print(f"'{col}': {column_mean} | {column_std} | {column_max} | {column_min}")
+    print(pd.DataFrame(list_of_columns))
+
+att_column();
+# print(data)
